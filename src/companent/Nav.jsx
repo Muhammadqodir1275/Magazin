7f0d6b62c006667d4a4dfb0e38b2bcbbf4c61334
Muhammadqodir1275 <muhammadqodir1275@gmail.com>
2025-05-16 14:21:52 +0500

    footer ozgardi

--- src/companent/Nav.jsx
+++ src/companent/Nav.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Carusel from "./Carousel";
 import Card from "./Card";
-import Footer from "./Footer";
+import Footer from "./footer";
 import Navbar from './navbar';
 const Nav = () => {
   return (
